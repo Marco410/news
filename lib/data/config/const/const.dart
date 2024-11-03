fa070628e210539992ff4b08b9a329222904a80f
Marco Castañeda <marco_pascual410@hotmail.com>
2024-11-02 23:50:11 -0600

    comment added

--- lib/data/config/const/const.dart
+++ lib/data/config/const/const.dart
@@ -1,5 +1,6 @@
 const String baseUrl = "newsapi.org";
-const String apiKey = "27e0bec5f162429a9ac8c91f1ed9d887";
+const String apiKey =
+    "27e0bec5f162429a9ac8c91f1ed9d887"; // for testing purposes
 
 const String articlesGet = "v2/everything";
 const String articlesWithCategoryGet = "v2/top-headlines";
